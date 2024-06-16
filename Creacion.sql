0df9c69488f45f6f4ab65578a86c9ac2dbd22a92
MrSpecter07 <bootsrecillas@gmail.com>
2024-06-15 21:02:56 -0600

    carlospm porque trabaja de noche

--- Creacion.sql
+++ Creacion.sql
@@ -1,16 +1,23 @@
 create database Autolavado;
+create table estado (
+	id_estado int primary key auto_increment,
+    estado varchar(20)
+);
+create table direccion (
+	id_direccion int primary key auto_increment,
+    id_estado int not null,
+    codigo_postal int not null,
+    calle varchar(250) not null,
+    numero_exterior int not null,
+    numero_interior int
+);
 create table sucursal (
 	id_sucursal int primary key auto_increment,
-    nombre varchar(255) not null,
-    estado varchar(20) not null,
-    colonia varchar(255) not null,
-    municipio varchar(255) not null,
-    codigo_postal int,
-    calle varchar(255) not null,
-    numero_exterior int not null
+    id_direccion int not null,
+    nombre varchar(255) not null
 );
 create table empleado (
-	curp char(18) primary key,
+	numero_empleado int primary key auto_increment,
     id_sucursal int not null,
     id_puesto int not null,
     nombre varchar(50) not null,
@@ -19,7 +26,7 @@ create table empleado (
 );
 create table cronograma (
 	id_cronograma int primary key auto_increment,
-    curp char(18) not null,
+    numero_empleado int not null,
     id_horario int not null
 );
 create table puesto (
@@ -30,8 +37,7 @@ create table puesto (
 create table horario (
 	id_horario int primary key auto_increment,
     hora_entrada time,
-    hora_salida time,
-    horas int
+    hora_salida time
 );
 create table contacto (
 	id_contacto int primary key auto_increment,
@@ -89,4 +95,14 @@ create table ticket (
     comentario varchar(250),
     subtotal decimal(8,2),
     total decimal(8,2)
+);
+create table compra (
+	id_compra int primary key auto_increment,
+    id_ticket int not null,
+    id_paquete int not null
+);
+create table promocion_sucursal (
+	id_promocion_sucursal int primary key auto_increment,
+    id_sucursal int not null,
+    id_promocion int not null
 );
